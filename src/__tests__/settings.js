cf16238f221b7bd24a0a917c24b9d969032f7ebc
Alexander Komlev <alexander.komlev42@gmail.com>
2024-07-28 13:49:07 +0700

    initial commit

---
+++ src/__tests__/settings.js
@@ -0,0 +1,28 @@
+import Settings from "../settings";
+
+test("Settings default test", () => {
+    const settings = new Settings();
+    expect(settings.settings).toEqual(new Map([["theme", "dark"], ["music", "trance"], ["difficulty", "easy"]]));
+});
+
+test("Settings set test", () => {
+    const settings = new Settings();
+    settings.set("theme", "light");
+    settings.set("music", "rock");
+    settings.set("difficulty", "easy");
+    expect(settings.settings).toEqual(new Map([["theme", "light"], ["music", "rock"], ["difficulty", "easy"]]));
+});
+
+test("Settings wrong set test", () => {
+    const settings = new Settings();
+    settings.set("theme", "wrong");
+    settings.set("music", "wrong");
+    settings.set("difficulty", "wrong");
+    expect(settings.settings).toEqual(new Map([["theme", "dark"], ["music", "trance"], ["difficulty", "easy"]]));
+});
+
+test("Settings param test", () => {
+    const settings = new Settings();
+    settings.set("language", "russian");
+    expect(settings.settings).toEqual(new Map([["theme", "dark"], ["music", "trance"], ["difficulty", "easy"]]));
+})
---
+++ src/settings.js
@@ -0,0 +1,38 @@
+export default class Settings {
+    constructor() {
+        this.defaultSettings = new Map([
+            ["theme", "dark"],
+            ["music", "trance"],
+            ["difficulty", "easy"]
+        ]);
+        this.userSettings = new Map();
+
+        Object.defineProperty(this, "settings", {
+            get: () => {
+                return new Map([...this.defaultSettings, ...this.userSettings])
+            }
+        });
+    }
+
+    set(key, value) {
+        switch (key) {
+            case "theme":
+                if (["light", "dark", "gray"].includes(value)) {
+                    this.userSettings.set(key, value);
+                }
+                break;
+            case "music":
+                if (['pop', 'rock', 'chillout', 'off', 'trance'].includes(value)) {
+                    this.userSettings.set(key, value);
+                }
+                break;
+            case "difficulty":
+                if (['easy', 'normal', 'hard', 'nightmare'].includes(value)) {
+                    this.userSettings.set(key, value);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
